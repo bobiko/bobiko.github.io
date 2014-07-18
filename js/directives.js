a689bcac86bcb4f442c04f395ae493544f85a0dd
bobiko <me@bobiko.pl>
2014-07-18 09:08:40 +0200

    & all bower pack

--- js/directives.js
+++ js/directives.js
@@ -4,8 +4,16 @@
 
 
 angular.module('bobikoApp.directives', []).
-  directive('appVersion', ['version', function(version) {
+directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+}]);
+directive('ngFileSelect',['data',  function(data) {
+    return function($scope, e) {
+        el.bind("change", function(e){
+            $scope.file = (e.srcElement || e.target).files[0];
+            $scope.getFile();
+        });
+    }
+}]);
